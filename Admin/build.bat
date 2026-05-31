a69d178fd1c428d8a0b1d972f7f7faf244b0ca0c
Mohmad Sahil <sahilsid2001@gmail.com>
2026-05-31 01:02:55 -0700

    -- Setup Done

--- Admin/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-node "node_modules\typescript\bin\tsc" -b
-if errorlevel 1 exit /b %errorlevel%
-node "node_modules\vite\bin\vite.js" build %*
--- Admin/src/hooks/useDashboard.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { dashboardService } from "@/services/dashboard.service";
-
-export function useDashboard() {
-  return useQuery({
-    queryKey: ["dashboard"],
-    queryFn: () => dashboardService.overview(),
-  });
-}
